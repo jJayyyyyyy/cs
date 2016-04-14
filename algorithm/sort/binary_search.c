a02704c8fe0216fef4b1b336efb0ba8927bffb2e
Steve <ljjdbd123@hotmail.com>
2016-04-14 20:57:05 +0800

    add <binary_search.c>

---
+++ algorithm/sort/binary_search.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// 用于升序排列的数组
+int binary_search(int a[], int left, int right, int key)
+{
+	if(left <= right){
+		int mid = (left + right) / 2;
+		if(key < a[mid]){
+			return binary_search(a, left, mid-1, key);
+		}else if(key > a[mid]){
+			return binary_search(a, mid+1, right, key);
+		}else{
+			return mid;
+		}
+	}
+
+	return -1;
+}
+
+int main()
+{
+	int a[] = {11, 24, 39, 41, 57, 86};
+	int len = sizeof(a)/sizeof(a[0]);
+
+	int index;
+	printf("The array is:\n");
+	for(index=0; index<len; index++){
+		printf("%d\t", a[index]);
+	}
+	printf("\n");
+	
+	int key=0;
+	printf("Please input the key: ");
+	scanf("%d", &key);
+
+	int index_of_key = binary_search(a, 0, len-1, key);
+	if(index_of_key >= 0){
+		printf("\nThe index of the key is: %d\n", index_of_key);
+	}else{
+		printf("Error! Not found!\n");
+	}
+}
