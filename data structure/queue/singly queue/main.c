0348cd706cf781f3c9fd9e7a61401f12cafe7844
Steve <ljjdbd123@hotmail.com>
2016-04-17 20:32:18 +0800

    add <Data Structure: singly queue>

---
+++ data structure/queue/singly queue/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include "queue.h"
+
+void print_all(QueueType *queue)
+{
+    printf("\nLength: %d. All data:\n", Get_Length_Queue(queue));
+    Traverse_Queue(queue);
+}
+
+void push(QueueType *queue, int n){
+    int index;
+    DATA data;
+
+    for(index=0; index<n; index++){
+        printf("\nInput data (id  name  age): \n");
+        scanf("%s%s%d", data.id, data.name, &data.age);
+        Push_Queue(queue, data);
+    }
+
+    print_all(queue);
+}
+
+void pop(QueueType *queue, int n){
+    int index;
+    DATA data;
+
+    printf("\nPop!!!\n");
+    for(index=0; index<n; index++){
+        data = Pop_Queue(queue);
+        printf("(%s, %s, %d)\n", data.id, data.name, data.age);
+    }
+
+    print_all(queue);
+}
+
+void get_back(QueueType *queue){
+    DATA data;
+
+    printf("\nGet Back!!!\n");
+    data = Check_Back_Queue(queue);
+    printf("(%s, %s, %d)\n", data.id, data.name, data.age);
+}
+
+void get_front(QueueType *queue){
+    DATA data;
+
+    printf("\nGet Front!!!\n");
+    data = Check_Front_Queue(queue);
+    printf("(%s, %s, %d)\n", data.id, data.name, data.age);
+}
+
+//main() for test
+int main()
+{
+    DATA data;
+    QueueType *queue;
+    int index;
+
+    queue = Init_Queue();
+    print_all(queue);
+
+    if(NULL != queue){
+        push(queue, 1);
+
+        pop(queue, 2);
+
+        get_back(queue);
+
+        get_front(queue);
+
+        printf("\nTest for <Is_Empty_Queue()>.\n");
+        for(index=0; index<3; index++){
+            Pop_Queue(queue);
+        }
+    }
+    printf("\n");
+
+    Free_Queue(queue);
+    return 0;
+}
---
+++ data structure/queue/singly queue/queue.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+//FIFO singly queue
+QueueType *Init_Queue()
+{
+    QueueType *queue;
+    queue = (QueueType *)malloc(sizeof(QueueType));
+    DATA data;
+
+    if(NULL == queue){
+        printf("Fail to allocate memory for a new Node!\n");
+        return NULL;
+    }
+
+    queue->front = 0;
+    queue->rear = 0;
+
+    printf("\nInput data (id  name  age). Quit when id=0. \n");
+    while(1){
+        fflush(stdin);
+        scanf("%s", data.id);
+        if(data.id[0]=='0' && data.id[1]=='\0')
+            break;
+        scanf("%s%d", data.name, &data.age);
+        queue = Push_Queue(queue, data);
+    }
+
+    if(!Is_Empty_Queue(queue))
+        printf("\nQueue generated!\n");
+    else
+        printf("\nQueue empty!\n");
+
+    return queue;
+}
+
+void Traverse_Queue(QueueType *queue)
+{
+    int i;
+    for(i=queue->front; i<queue->rear; i++){
+        printf("(%s, %s, %d)\n", queue->data[i].id, queue->data[i].name, queue->data[i].age);
+    }
+}
+
+//push new element to the rear of queue
+QueueType *Push_Queue(QueueType *queue, DATA data)
+{
+    if(Is_Full_Queue(queue)){
+        printf("\nFailed! Queue is full!\n");
+        return 0;
+    }
+
+    queue->data[queue->rear++] = data;
+    return queue;
+}
+
+//pop element from the front of queue
+DATA Pop_Queue(QueueType *queue)
+{
+    if(Is_Empty_Queue(queue)){
+        printf("Failed! Queue is empty!\n");
+        exit(0);
+    }
+
+    return (queue->data[queue->front++]);
+}
+
+DATA Check_Back_Queue(QueueType *queue)
+{
+    if(Is_Full_Queue(queue)){
+        printf("Failed! Queue is full!\n");
+        exit(0);
+    }
+
+    return (queue->data[queue->rear-1]);
+}
+
+DATA Check_Front_Queue(QueueType *queue)
+{
+    if(Is_Empty_Queue(queue)){
+        printf("Failed! Queue is empty!\n");
+        exit(0);
+    }
+
+    return (queue->data[queue->front]);
+}
+
+int Is_Empty_Queue(QueueType *queue)
+{
+    return (queue->front == queue->rear);
+}
+
+int Is_Full_Queue(QueueType *queue)
+{
+    return (QUEUEMAX == queue->rear);
+}
+
+int Get_Length_Queue(QueueType *queue)
+{
+    return (queue->rear - queue->front);
+}
+
+int Free_Queue(QueueType *queue)
+{
+    if(NULL != queue)
+        free(queue);
+    return 1;
+}
