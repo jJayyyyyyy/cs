d1dcfd913425eef88a9cfe8502046885819735de
Steve <ljjdbd123@hotmail.com>
2017-01-06 17:48:56 +0800

    rename to <translate_web_xml.py>, use input() to keep the same api, sys.argv[] is optional.

--- just for fun/youdao_translation/translate_web_xml.py
+++ just for fun/youdao_translation/translate_web_xml.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from urllib import request, parse
-import sys, re
+import re
+# import sys
 
-word = parse.quote(sys.argv[1])
+word = parse.quote(input())
+# word = parse.quote(sys.argv[1])
 url = 'http://dict.youdao.com/fsearch?q=' + word
 
-req = request.Request(url)
-resp = request.urlopen(req).read().decode('utf-8')
-
+resp = request.urlopen(url).read().decode('utf-8')
 data = re.findall(r'<content><\!\[CDATA\[(.*)\]\]>', resp)
 
 for i in data:
