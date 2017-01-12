096e496ed52a10547329ac10016c2a431f7ce9e5
Steve <ljjdbd123@hotmail.com>
2017-01-12 21:20:51 +0800

    optimize algorithm

--- math/prime/is_prime.c
+++ math/prime/is_prime.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 
-int is_prime(int num)
-{
-    int i, num_remainder;
-    for(i=2; i<num; i++){
-        num_remainder = num % i;
-        if(0 == num_remainder){
-            return 0;
-        }
-    }
-
-    return 1;
+int is_prime(int num){
+	int divisor=1, up_bound=1;
+	if( 2<num && num%2){
+		up_bound = (int)sqrt(num);
+		for(divisor=3; divisor<=up_bound; divisor+=2){
+			if( 0 == num % divisor )
+				return 0;
+		}
+		return 1;
+	}else if( 2==num )
+		return 1;
+	else
+		return 0;
 }
 
-int main()
-{
-    int num;
-    printf("Please input a number: ");
-    scanf("%d", &num);
-    (is_prime(num)==1)?printf("yes\n"):printf("no\n");
+int main(){
+	int num;
+	printf("Please input a number: ");
+	scanf("%d", &num);
+	(is_prime(num)==1)?printf("yes\n"):printf("no\n");
 
-    return 1;
+return 1;
 }
