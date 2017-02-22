70bfcc5bb90fcc75ac7670b19a34acadfd31cb43
Steve <ljjdbd123@hotmail.com>
2017-02-22 12:01:38 +0800

    add readme and update source file for PAT_B_1020

--- OJ/PAT/basic_level/1020_月饼/mooncake.cpp
+++ OJ/PAT/basic_level/1020_月饼/mooncake.cpp
@@ -1,15 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
 #define MAX 1004
+using namespace std;
 
-typedef struct{
+struct Cake{
 	double stock;
 	double totalPrice;
 	double unitPrice;
-}Cake;
+};
 
-int getCakes(Cake cakes[], int n){
-	int i=0;
+int getCakes(Cake *cakes, int n){
+	int i;
+
 	for( i=0; i<n; i++ ){
 		scanf("%lf", &cakes[i].stock);
 	}
@@ -22,29 +25,20 @@ int getCakes(Cake cakes[], int n){
 	return 0;
 }
 
-int decCmp(const void *p1, const void *p2){
-	const Cake *a1 = (const Cake *)p1;
-	const Cake *a2 = (const Cake *)p2;
-
-	if( a1->unitPrice > a2->unitPrice )
-		return -1;
-	else if( a1->unitPrice==a2->unitPrice )
-		return 0;
-	else
-		return 1;
+int decCmp(Cake a, Cake b){
+	return a.unitPrice > b.unitPrice;
 }
 
 int main(){
-	int us_N=0, demand=0;
+	int us_N, demand=0;
 	double maxPrice=0.0;
 	Cake cakes[MAX];
-	int i=0;
 
 	scanf("%d%d", &us_N, &demand);
 	getCakes(cakes, us_N);
-	qsort(cakes, us_N, sizeof(Cake), decCmp);
+	sort(cakes, cakes+us_N, decCmp);
 
-	for( i=0; i<us_N; i++ ){
+	for( int i=0; i<us_N; i++ ){
 		if( demand>=cakes[i].stock ){
 			maxPrice += cakes[i].totalPrice;
 			demand -= cakes[i].stock;
@@ -54,6 +48,6 @@ int main(){
 		}
 	}
 
-	printf("%.2lf\n", maxPrice);
+	printf("%.2f\n", maxPrice);
 	return 0;
 }
