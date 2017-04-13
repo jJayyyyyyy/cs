9ad694fc365f94c82607d53d15dab478e766bd18
FrozenMap <ljjdbd123@hotmail.com>
2017-04-13 13:16:56 +0000

    rewrite PAT_A_1020

--- OJ/PAT/advanced_level/1020_Tree_Traversals/tree_traversals.cpp
+++ OJ/PAT/advanced_level/1020_Tree_Traversals/tree_traversals.cpp
@@ -4,42 +4,48 @@
 using namespace std;
 
 int postOrder[MAX], inOrder[MAX];
-int n, isFirst=1;
+int isFirst = 1;
 
-struct Node{
+class Node{
+public:
 	Node *lchild;
 	Node *rchild;
 	int value;
-	Node(int v){
+	Node(int _value){
+		value = _value;
 		lchild = NULL;
 		rchild = NULL;
-		value = v;
 	}
 };
 
-Node *insert(int pLeft, int pRight, int inLeft, int inRight){
-	if( pLeft>pRight )
+Node *insert(int postLeft, int postRight, int inLeft, int inRight){
+	if( postLeft > postRight ){
 		return NULL;
-
-	int parentIndex = pRight;
+	}
+	int parentIndex = postRight;
 	int parentValue = postOrder[parentIndex];
 	Node *parent = new Node(parentValue);
 
 	int mid;
-	for( mid=inLeft; mid<inRight; mid++ ){
-		if( parentValue==inOrder[mid] ){
+	for( mid=inLeft; mid<=inRight; mid++ ){
+		if( inOrder[mid]==parentValue ){
 			break;
 		}
 	}
+	int cntLeftTree = mid - inLeft;
 
-	int cntLeft = mid - inLeft;
-	int pSubRight = pLeft + cntLeft - 1;
-	int pSubLeft = pLeft + cntLeft;
-	int inSubRight = mid - 1;
-	int inSubLeft = mid + 1;
+	int postLeftTreeStart = postLeft;
+	int postLeftTreeEnd = postLeft + cntLeftTree - 1;
+	int postRightTreeStart = postLeft + cntLeftTree;
+	int postRightTreeEnd = postRight - 1;
+	
+	int inLeftTreeStart = inLeft;
+	int inLeftTreeEnd = mid - 1;
+	int inRightTreeStart = mid + 1;
+	int inRightTreeEnd = inRight;
 
-	parent->lchild = insert( pLeft, pSubRight, inLeft, inSubRight );
-	parent->rchild = insert( pSubLeft, pRight-1, inSubLeft, inRight );
+	parent->lchild = insert(postLeftTreeStart, postLeftTreeEnd, inLeftTreeStart, inLeftTreeEnd);
+	parent->rchild = insert(postRightTreeStart, postRightTreeEnd, inRightTreeStart, inRightTreeEnd);
 	return parent;
 }
 
@@ -56,23 +62,25 @@ int output(int value){
 
 int levelTraverse(Node *root){
 	queue<Node*> q;
-	q.push(root);
-
+	q.push( root );
 	while( q.size() ){
 		Node *parent = q.front();
-		output(parent->value);
 		q.pop();
-		if( NULL!=parent->lchild )
-			q.push(parent->lchild);
-		if( NULL!=parent->rchild )
-			q.push(parent->rchild);
+		output(parent->value);
+		if( NULL != parent->lchild ){
+			q.push( parent->lchild );
+		}
+		if( NULL != parent->rchild ){
+			q.push( parent->rchild );
+		}
 	}
 	return 0;
 }
 
 int freeTree(Node *root){
-	if(root==NULL)
+	if( NULL == root ){
 		return 0;
+	}
 	freeTree(root->lchild);
 	freeTree(root->rchild);
 	delete root;
@@ -80,17 +88,18 @@ int freeTree(Node *root){
 }
 
 int main(){
+	int n, i;
 	cin>>n;
-	int i;
+
 	for( i=0; i<n; i++ ){
 		cin>>postOrder[i];
 	}
 	for( i=0; i<n; i++ ){
 		cin>>inOrder[i];
 	}
-
-	Node *root = insert(0, n-1, 0, n-1);
+	Node *root = insert( 0, n-1, 0, n-1 );
 	levelTraverse(root);
 	freeTree(root);
+
 	return 0;
 }
