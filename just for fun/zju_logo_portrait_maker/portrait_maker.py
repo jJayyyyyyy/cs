162306be35e047836bcc1337f8fce0995120079c
FrozenMap <ljjdbd123@hotmail.com>
2017-06-30 01:06:54 +0000

    update [portrait_maker.py], functionized

--- just for fun/zju_logo_portrait_maker/portrait_maker.py
+++ just for fun/zju_logo_portrait_maker/portrait_maker.py
@@ -1,70 +1,81 @@
 from PIL import Image
 import sys
 
-portrait = 'portrait.jpeg'
-logo = 'logo.png'
+# RGBA-white, which is RGB-white with alpha-0
+white = (255, 255, 255, 0)
 
-def circle():
-	old_img = Image.open(logo).convert("RGBA")
-	size = old_img.size
-	diameter = min(size[0], size[1])
-	if size[0] != size[1]:
-		size = (diameter, diameter)
-		old_img = old_img.resize( size, Image.ANTIALIAS)
+def get_square(img):
+	size = img.size
+	length, width = size[0:2]
+	edgeLength = max(length, width)
+	newSize = (edgeLength, edgeLength)
+	square = Image.new('RGBA', newSize, white)	# RGBA
+	region = ( (edgeLength - length)//2, (edgeLength - width)//2 )
+	square.paste(img, region)
+	print('sqaure size: ', square.size)
+	square.save('sqaure.png', 'png')
+
+	return square
+
+def resize_640(img):
+	size = img.size
+	length, width = size[0:2]
+	newSize = (640, 640)
+	newImage = img.resize(newSize, Image.ANTIALIAS)
+	print('resized image: ', newImage.size)
+	newImage.save('square_640.png', 'png')
 	
-	rgb_white = (255, 255, 255, 0)
+	return newImage
+
+def get_frame(img):
+	size = img.size
+	newSize = (800, 800)
+	square = Image.new('RGBA', newSize, 'white')	# RGBA, but 'white' to add white-color frame
+	region = (80, 80)
+	square.paste(img, region)
+	print('size with frame: ', square.size)
+	square.save('square_800_with_frame.png', 'png')
+
+	return square
+
+def get_mask():
+	# logo.png should be a 200*200 png image
+	logo = Image.open('logo.png').convert('RGBA')
+	logoSize = logo.size
+	diameter = logoSize[0]
+	white = (255, 255, 255, 0)
 	size = (diameter, diameter)
 
-	white_img = Image.new('RGBA', size, rgb_white)
-
-	old_img_load = old_img.load()
-	white_img_load = white_img.load()
-	
+	# background
+	bg = Image.new('RGBA', size, white)
 	radius = diameter / 2
 	center = 0.0 + float(diameter/2)
+	logoLoad = logo.load()
+	bgLoad = bg.load()
 
 	for i in range(diameter):
 		for j in range(diameter):
 			x = abs(i - center + 0.5)
 			y = abs(j - center + 0.5)
-			l = pow(x, 2) + pow(y, 2)
-			if l <= pow(radius, 2):
-				white_img_load[i, j] = old_img_load[i, j]
-	return white_img
-
-
-def get_portrait(filename):
-	old_img = Image.open(filename)
-	old_size = old_img.size
-
-	new_size = (old_size[0] + 200, old_size[1] + 200)
-	new_img = Image.new('RGB', new_size, 'white')
-
-	region = ( (new_size[0] - old_size[0])//2, (new_size[1] - old_size[1])//2 )
-
-	new_img.paste(old_img, region)
-
-	mask = circle()
-	a = mask.split()[3]
-	box = (old_size[0], old_size[1], new_size[0], new_size[1])
-	new_img.paste(mask, box, mask=a)
-
-	return new_img
+			dis = pow(x, 2) + pow(y, 2)
+			if dis <= pow(radius, 2):
+				bgLoad[i, j] = logoLoad[i, j]
+	return bg
 
 def main():
 	filename = sys.argv[1]
-	portrait = get_portrait(filename)
+	img = Image.open(filename)
+	square = get_square(img)
+	square640 = resize_640(square)
+	square800 = get_frame(square640)
+	
+	portrait = square800
+	mask = get_mask()
+	alpha = mask.split()[3]
+	region = (600, 600)
+	portrait.paste(mask, region, mask=alpha)
 	portrait.show()
-	# portrait.save('new_portrait.png', 'png')
+	portrait.save('portrait.png', 'png')
 
-main()
-
-def others():
-	# r = 118
-	# border = (700-r, 700-r, 700+r, 700+r)
-
-	# draw = ImageDraw.Draw(new_img)
-	# draw.ellipse( border, fill=(255, 255, 255, 0)  )
-
-	# mask = Image.open('logo.png')
-	pass
+if __name__ == '__main__':
+	main()
