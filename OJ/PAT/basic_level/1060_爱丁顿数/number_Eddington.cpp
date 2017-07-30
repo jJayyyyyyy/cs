5b410f9f3e736a729a5322059c92a3f0df6ff8e4
FrozenMap <ljjdbd123@hotmail.com>
2017-07-30 07:39:31 +0000

    update PAT_B_1060

--- OJ/PAT/basic_level/1060_爱丁顿数/number_Eddington.cpp
+++ OJ/PAT/basic_level/1060_爱丁顿数/number_Eddington.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int num[MAXSIZE]={0};
 
 int cmp(int a, int b){
-	return a>b;
+	return a > b;
 }
 
 int main(){
@@ -13,33 +13,17 @@ int main(){
 	cin.tie(0);
 	int n, i;
 	cin>>n;
-
-	for( i=1; i<=n; i++ ){
+	for( i = 1; i <= n; ++i ){
 		cin>>num[i];
 	}
-	sort(&num[1], &num[n+1], cmp);
+	sort(num + 1, num + n + 1, cmp);	// 关键在这里，没说要按照日期来，只是说给定这么一段时间，求出爱丁顿数e就好了
+										// 因此不妨从大到小排序
 
-	int e=1;
-	while(1){
-		if( e<100000 && num[e]>e ){
-			e *= 2;
-		}else{
-			e = e/2; 
-			break;
-		}
+	int e = 0;
+	while( e < n && num[e+1] > e+1 ){
+		++e;
 	}
-
-	while(e<=100000){
-		if( num[e]<=e ){
-			break;
-		}
-		e++;
-	}
-
-	if(e==0)
-		cout<<0;
-	else
-		cout<<e-1;
+	cout<<e<<'\n';
 
 	return 0;
 }
