ffac4e02d1df1a3c5c866889e0fa9422d6112a9b
FrozenMap <ljjdbd123@hotmail.com>
2017-08-29 11:22:40 +0000

    add gcd_lcm

---
+++ algorithm/gcd_lcm/gcd_lcm.cpp
@@ -0,0 +1,21 @@
+// PAT_B_1062, 1034
+// PAT_A_1081
+
+// 最大公因数, the Greatest Common Divisor
+int get_gcd(int a, int b){
+	if( b == 0 ){
+		return a;
+	}else{
+		return get_gcd(b, a % b);
+	}
+}
+
+// 最小公倍数, the Lowest Common Multiple
+int get_lcm(int a, int b){
+	int gcd = get_gcd(a, b);
+	if( gcd != 0 ){
+		return a * b / get_gcd(a, b)
+	}else{
+		return 0;
+	}
+}
