859aa910dc525e0a10325d8c6ba63e59c9b0d434
FrozenMap <ljjdbd123@hotmail.com>
2017-09-06 13:04:33 +0000

    update PAT_1091

--- OJ/PAT/advanced_level/1091_Acute_Stroke/acute_stroke.cpp
+++ OJ/PAT/advanced_level/1091_Acute_Stroke/acute_stroke.cpp
@@ -1,62 +1,56 @@
 #include <iostream>
 #include <queue>
-#define ZSIZE 64
-#define XSIZE 1300
-#define YSIZE 160
+#define MAXZ 64
+#define MAXX 1290
+#define MAXY 130
 using namespace std;
+
+int sizeZ, sizeX, sizeY, TH;
+bool pixel[MAXZ][MAXX][MAXY] = {false};
+bool vis[MAXZ][MAXX][MAXY] = {false};
 
 struct Node{
 	int z, x, y;
-	Node(int _z, int _x , int _y){
-		z = _z;
-		x = _x;
-		y = _y;
-	}
+	Node(int _z, int _x, int _y): z(_z), x(_x), y(_y){}
 };
 
-int m, n, slice, th;
-int pixel[ZSIZE][XSIZE][YSIZE];	// 内存够用，尽量选int，比bool要快一些
-bool inq[ZSIZE][XSIZE][YSIZE] = {false};
-
-int incZ[6] = {1, -1, 0, 0, 0, 0};
-int incX[6] = {0, 0, 1, -1, 0, 0};
-int incY[6] = {0, 0, 0, 0, 1, -1};
-
 bool judge(int z, int x, int y){
-	if( z >= slice || z < 0 || x >= m || x < 0 || y >= n || y < 0 ){
+	if( z >= sizeZ || z < 0 || x >= sizeX || x < 0 || y >= sizeY || y < 0 ){
 		return false;
-	}
-	if( pixel[z][x][y] == 0 || inq[z][x][y] == true ){
+	}else if( pixel[z][x][y] == false || vis[z][x][y] == true ){
 		return false;
+	}else{
+		return true;
 	}
-	return true;
 }
 
 int BFS(int z, int x, int y){
-	int volume = 0;
+	int incZ[6] = {1, -1, 0, 0, 0, 0};
+	int incX[6] = {0, 0, 1, -1, 0, 0};
+	int incY[6] = {0, 0, 0, 0, 1, -1};
+	int cnt = 0;
 	queue<Node> q;
 	Node node(z, x, y);
 	q.push(node);
-	inq[z][x][y] = true;
+	vis[z][x][y] = true;
 
 	while( q.size() ){
 		Node now = q.front();
 		q.pop();
-		++volume;
-		for( int i=0; i<6; ++i ){
-			int newZ = now.z + incZ[i];
-			int newX = now.x + incX[i];
-			int newY = now.y + incY[i];
-			if( judge(newZ, newX, newY) ){
-				Node n(newZ, newX, newY);
-				q.push(n);
-				inq[newZ][newX][newY] = true;
+		++cnt;
+		for( int i = 0; i < 6; ++i ){
+			int nextZ = now.z + incZ[i];
+			int nextX = now.x + incX[i];
+			int nextY = now.y + incY[i];
+			if( judge(nextZ, nextX, nextY) ){
+				Node next(nextZ, nextX, nextY);
+				q.push(next);
+				vis[nextZ][nextX][nextY] = true;
 			}
 		}
 	}
-
-	if( volume >= th ){
-		return volume;
+	if( cnt >= TH ){
+		return cnt;
 	}else{
 		return 0;
 	}
@@ -65,30 +59,24 @@ int BFS(int z, int x, int y){
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-
-	cin>>m>>n>>slice>>th;
-	
-	for( int z=0; z < slice; ++z ){
-		for( int x=0; x<m; ++x){
-			for(int y=0; y<n; ++y){
-				cin>>pixel[z][x][y];	// 坐标形式要注意，首先是第z层，然后是该层内的坐标[row][col]，即[x][y]
+	cin>>sizeX>>sizeY>>sizeZ>>TH;
+	for( int z = 0; z < sizeZ; ++z ){
+		for( int x = 0; x < sizeX; ++x ){
+			for( int y = 0; y < sizeY; ++y ){
+				cin>>pixel[z][x][y];
 			}
 		}
 	}
-	
-	int totalVolume=0;
-
-	for( int z=0; z < slice; ++z ){
-		for( int x=0; x<m; ++x){
-			for(int y=0; y<n; ++y){
-				if( pixel[z][x][y] == 1 && inq[z][x][y] == false ){
-					totalVolume += BFS(z, x, y);
+	int totalVol = 0;
+	for( int z = 0; z < sizeZ; ++z ){
+		for( int x = 0; x < sizeX; ++x ){
+			for( int y = 0; y < sizeY; ++y ){
+				if( pixel[z][x][y] == true && vis[z][x][y] == false ){
+					totalVol += BFS(z, x, y);
 				}
 			}
 		}
 	}
-
-	cout<<totalVolume<<'\n';
-
+	cout<<totalVol<<'\n';
 	return 0;
 }
