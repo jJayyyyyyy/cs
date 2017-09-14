d945583b575bdcd768ada2ebb4f1a9a8cd6ad9a3
FrozenMap <ljjdbd123@hotmail.com>
2017-09-14 08:42:04 +0000

    update UFS template in PAT_1118

--- OJ/PAT/advanced_level/1118_Birds_in_Forest/birds_in_forest.cpp
+++ OJ/PAT/advanced_level/1118_Birds_in_Forest/birds_in_forest.cpp
@@ -2,7 +2,7 @@
 #define MAXSIZE 10004
 using namespace std;
 
-bool marked[MAXSIZE] = {false};
+bool vis[MAXSIZE] = {false};
 int father[MAXSIZE];
 int treeList[MAXSIZE] = {0};
 
@@ -20,8 +20,8 @@ int findFather(int x){
 }
 
 void Union(int a, int b){
-	int fa1 = father[a];
-	int fa2 = father[b];
+	int fa1 = findFather(a);
+	int fa2 = findFather(b);
 	if( fa1 < fa2 ){
 		father[fa2] = fa1;
 	}else{
@@ -29,52 +29,57 @@ void Union(int a, int b){
 	}
 }
 
-int main(){
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int n, i, j, k, rootID, id, cntTree=0, maxID=0, q, id1, id2;
-	init();
-	cin>>n;
-	for( i = 0; i < n; ++i ){
-		cin>>k;
-		cin>>rootID;
-		marked[rootID] = true;
-		if( rootID > maxID ){
-			maxID = rootID;
-		}
-		for( j = 1; j < k; ++j ){
-			cin>>id;
-			marked[id] = true;
-			if( id > maxID ){
-				maxID = id;
-			}
-			Union(rootID, id);
-		}
-	}	
-
-	for( i = 0; i < MAXSIZE; ++i ){
-		if( marked[i] == true ){
-			rootID = findFather(i);
-			treeList[rootID]++;
+// 统计区块，以及maxID
+void cntTreesAndBirds(){
+	int cntBirds = 0;	// ~maxID
+	for( int i = 1; i < MAXSIZE; ++i ){
+		if( vis[i] == true ){
+			cntBirds = i;			// 更新maxID
+			int rootID = findFather(i);
+			treeList[rootID]++;		// treeList[rootID] 的值表示这个区块里面有多少个结点
 		}
 	}
-
-	for( i = 0; i < MAXSIZE; ++i ){
+	int cntTree = 0;
+	for( int i = 0; i < MAXSIZE; ++i ){
 		if( treeList[i] > 0 ){
-			++cntTree;
+			cntTree++;				// treeList[i] 不为0，说明这是一个区块，i标识了该区块，即rootID
 		}
 	}
+	cout<<cntTree<<' '<<cntBirds<<'\n';
+}
 
-	cout<<cntTree<<' '<<maxID<<'\n';
+void isSameTree(){
+	int q, id1, id2;
 	cin>>q;
-	for( i = 0; i < q; ++i ){
+	for( int i = 0; i < q; ++i ){
 		cin>>id1>>id2;
-		if( findFather(id1) == findFather(id2) ){
+		if( findFather(id1) == findFather(id2) ){	// 千万注意，判断是否在同一区块，要用findFather(), 而不是father[]是否相等（除非进行了路径优化）
 			cout<<"Yes\n";
 		}else{
 			cout<<"No\n";
 		}
 	}
+}
 
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	init();
+	int n, k, id1, id2;
+	cin>>n;
+	for( int i = 0; i < n; ++i ){
+		cin>>k;
+		if( k > 0 ){
+			cin>>id1;
+			vis[id1] = true;		// 输入的时候，进行vis[id] = true
+			for( int j = 1; j < k; ++j ){
+				cin>>id2;
+				vis[id2] = true;	// 输入的时候，进行vis[id] = true
+				Union(id1, id2);	// 输入的时候进行Union(id1, id2)
+			}
+		}
+	}
+	cntTreesAndBirds();
+	isSameTree();
 	return 0;
 }
