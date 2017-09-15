c0972548ebe9a3f1ebf88f8552de550df2f992dc
FrozenMap <ljjdbd123@hotmail.com>
2017-09-15 06:49:31 +0000

    update UFS template

--- data structure/union_find_set/UFS_template.cpp
+++ data structure/union_find_set/UFS_template.cpp
@@ -1,56 +1,54 @@
-struct Family{
-    int rootID = 0;     // 根节点编号
-    int people = 0;     // 每个区块/家族的成员数量
-}
+#include <iostream>
+#define MAXSIZE 1004
+using namespace std;
 
-Family family[MAXSIZE];
-bool marked[MAXSIZE] = {false}; // 如果输入中包含了i, 那么对应的marked[i]就标为true
+bool vis[MAXSIZE] = {false}; // 输入时，将对应的 vis[i] 标为 true
 int father[MAXSIZE];
+int blockList[MAXSIZE] = {0};
 
 // 初始化
 void initFather(){
-    for( int i = 0; i < MAXSIZE; ++i ){
-        father[i] = i;
-    }
+	for( int i = 0; i < MAXSIZE; ++i ){
+		father[i] = i;
+	}
 }
 
-// 省略路径压缩，好记
+// 此处省略了路径压缩，好记
 int findFather(int x){
-    while( x != father[x] ){
-        x = father[x];
-    }
-    return x;
+	while( x != father[x] ){
+		x = father[x];
+	}
+	return x;
 }
 
 // 归并为同一集合
 void Union(int a, int b){
-    int faA = findFather(a);
-    int faB = findFather(b);
-
-    // 本题要求以家族中最小编号为代表，即可以最小编号为根
-    if( faA < faB ){
-        // 确保根节点的编号是最小的
-        father[faB] = faA;
-    }else{
-        // 确保根节点的编号是最小的
-        father[faA] = faB;
-    }
+	int fa1 = findFather(a);
+	int fa2 = findFather(b);
+	// 以最小编号为根
+	if( fa1 < fa2 ){
+		father[fa2] = fa1;
+	}else{
+		father[fa1] = fa2;
+	}
 }
 
-// 统计区块component
-int cntFamily(){
-    for( int i = 0; i < MAXSIZE; ++i ){
-        if( marked[i] == true ){
-            int rootID = findFather(i);
-            family[rootID].rootID = rootID;
-            family[rootID].people++;
-        }
-    }
-    int cnt = 0;
-    for( int i = 0; i < MAXSIZE; ++i ){
-        if( family[i].people > 0 ){
-            cnt++;
-        }
-    }
-    return cnt;
+// PAT_1118 统计区块数量，以及maxID
+void cntBlockAndMaxID(){
+	int maxID = 0;
+	for( int i = 0; i < MAXSIZE; ++i ){
+		if( vis[i] == true ){
+			maxID = i;			// 最大结点编号
+			int rootID = findFather(i);
+			blockList[rootID]++;
+		}
+	}
+	int block = 0, maxBlock = -1;
+	for( int i = 0; i < MAXSIZE; ++i ){
+		if( blockList[i] > 0 ){
+			maxBlock = i;		// 最大区块编号，可用于对区块进行排序
+			block++;			// 最大区块数量
+		}
+	}
+	cout<<block<<' '<<maxID<<'\n';
 }
