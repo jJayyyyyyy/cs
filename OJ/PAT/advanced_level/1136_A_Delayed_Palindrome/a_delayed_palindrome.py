f5b164c4bb26cd1be1a34e6d313de7b1fe393a08
FrozenMap <ljjdbd123@hotmail.com>
2017-12-30 13:35:00 +0000

    update PAT_1136

--- OJ/PAT/advanced_level/1136_A_Delayed_Palindrome/a_delayed_palindrome.py
+++ OJ/PAT/advanced_level/1136_A_Delayed_Palindrome/a_delayed_palindrome.py
@@ -1,23 +1,21 @@
-def get_rev(num):
-	return int((str(num))[::-1])
+# 用python最简单，不用考虑大整数运算溢出
 
 def main():
-	s = input()
-	num = int(s)
-	rnum = get_rev(num);
-
-	for cnt in range(10):
-		if num == rnum:
+	s1 = input()
+	flag = 0
+	for i in range(10):
+		s2 = s1[::-1]
+		n1, n2 = int(s1), int(s2)
+		if n1 == n2:
+			print("%d is a palindromic number." % n1)
+			flag = 1
 			break
 		else:
-			res = num + rnum
-			print("%d + %d = %d" % (num, rnum, res))
-			num = res
-			rnum = get_rev(num)
-
-	if num == rnum:
-		print("%d is a palindromic number." % num)
-	else:
+			n3 = n1 + n2
+			s3 = str(n3)
+			print("%d + %d = %d" % (n1, n2, n3))
+			s1 = s3
+	if flag == 0:
 		print("Not found in 10 iterations.")
 
 main()
