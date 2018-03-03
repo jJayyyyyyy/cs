63afd8cc7061a9078b3fbb1169b5523e0a611a69
FrozenMap <ljjdbd123@hotmail.com>
2018-03-03 09:25:00 +0000

    update PAT_1127

--- OJ/PAT/advanced_level/1127_ZigZagging_on_a_Tree/zigzagging_on_a_tree.cpp
+++ OJ/PAT/advanced_level/1127_ZigZagging_on_a_Tree/zigzagging_on_a_tree.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
-#include <string>
 #include <stack>
 #include <vector>
 #define MAXSIZE 32
@@ -9,86 +9,102 @@ using namespace std;
 struct Node{
 	int val, level;
 	Node *lchild, *rchild;
-	Node(int _val){
-		val = _val;
+	Node(int v){
+		val = v;
 		level = 1;
 		lchild = rchild = NULL;
 	}
 };
 
-int post[MAXSIZE]={0}, in[MAXSIZE]={0};
-int n;
-vector<Node *> ans;
+int in[MAXSIZE], post[MAXSIZE];
+vector<Node *> levelList;
 stack<int> st;
+vector<int> ans;
 
-void r_output(){
-	while( st.size() ){
-		cout<<' '<<st.top();
+void getAllStack(){
+	while( st.size() != 0 ){
+		ans.push_back(st.top());
 		st.pop();
 	}
 }
 
-void output(){
-	cout<<ans[0]->val;
-	for( int i = 1; i < n; ++i ){
-		if( ans[i]->level % 2 == 0 ){
-			r_output();
-			cout<<' '<<ans[i]->val;
+void zigzag(){
+	for( auto node : levelList ){
+		if( node->level % 2 == 1 ){
+			st.push(node->val);
 		}else{
-			st.push(ans[i]->val);
+			getAllStack();
+			ans.push_back(node->val);
 		}
 	}
-	r_output();
-	cout<<'\n';
+	getAllStack();
 }
 
-void levelTraverse(Node * root){
+void vis(Node * node){
+	levelList.push_back(node);
+}
+
+void levelTrav(Node * root){
 	queue<Node *> q;
 	q.push(root);
 	while( q.size() ){
 		Node * node = q.front();
-		ans.push_back(node);
+		vis(node);
 		q.pop();
-		if( NULL != node->lchild ){
+		if( node->lchild != NULL ){
 			node->lchild->level = node->level + 1;
 			q.push(node->lchild);
 		}
-		if( NULL != node->rchild ){
+		if( node->rchild != NULL ){
 			node->rchild->level = node->level + 1;
 			q.push(node->rchild);
 		}
 	}
-	output();
 }
 
-void insert(Node * & root, int postL, int postR, int inL, int inR){
+Node * inPost(int inL, int inR, int postL, int postR){
 	if( postL > postR ){
-		return;
+		return NULL;
 	}
 	int val = post[postR];
-	root = new Node(val);
+	Node * root = new Node(val);
 
 	int mid;
-	for( mid = inL; mid <= inR; ++mid ){
+	for( mid = inL; mid <= inR; mid++ ){
 		if( in[mid] == val ){
 			break;
 		}
 	}
 	int numLeft = mid - inL;
-	insert(root->lchild, postL, postL+numLeft-1, inL, mid-1);
-	insert(root->rchild, postL+numLeft, postR-1, mid+1, inR);
+	root->lchild = inPost(inL, mid-1, postL, postL+numLeft-1);
+	root->rchild = inPost(mid+1, inR, postL+numLeft, postR-1);
+	return root;
+}
+
+void printAns(){
+	cout<<ans[0];
+	for( int i = 1; i < ans.size(); i++ ){
+		cout<<' '<<ans[i];
+	}
+	cout<<'\n';
 }
 
 int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
 	cin>>n;
-	for( int i = 0; i < n; ++i ){
+
+	for( int i = 0; i < n; i++ ){
 		cin>>in[i];
 	}
-	for( int i = 0; i < n; ++i ){
+	for( int i = 0; i < n; i++ ){
 		cin>>post[i];
 	}
-	Node * root = NULL;
-	insert(root, 0, n-1, 0, n-1);
-	levelTraverse(root);
+
+	Node * root = inPost(0, n-1, 0, n-1);
+	levelTrav(root);
+	zigzag();
+	printAns();
 	return 0;
 }
