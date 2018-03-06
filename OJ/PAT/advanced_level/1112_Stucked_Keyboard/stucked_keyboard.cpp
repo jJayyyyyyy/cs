e0d221230568c83e8704be9c6b725ab6cbd2e48d
FrozenMap <ljjdbd123@hotmail.com>
2018-03-06 07:45:30 +0000

    update PAT_A_1112

--- OJ/PAT/advanced_level/1112_Stucked_Keyboard/stucked_keyboard.cpp
+++ OJ/PAT/advanced_level/1112_Stucked_Keyboard/stucked_keyboard.cpp
@@ -1,67 +1,66 @@
 #include <iostream>
 #include <string>
-#include <set>
 using namespace std;
 
-bool sureNotBroken[160] = {false};
-bool sureBroken[160] = {false};
-set<char> printed;
+bool notBroken[160] = {false};
+bool broken[160] = {false};
+bool printed[160] = {false};
 
-int main() {
-	int k, i, cnt = 1;
-	string s, sEnd;
+int main(){
+	int k, cnt = 1;
+	string s;
 	cin>>k>>s;
-	
-	char pre='^', cursor;
-	sEnd = s + '$';
 
-	for( i=0; i<sEnd.length(); i++ ){
-		cursor = sEnd[i];
+	char pre='^', cursor;
+	char end='$';
+	s = s + end;
+	int len = s.size();
+
+	for( int i = 0; i < len; i++ ){
+		cursor = s[i];
 		if( cursor == pre ){
 			cnt++;
 		}else{
 			if( cnt % k != 0 ){
-				sureNotBroken[pre] = true;
+				notBroken[pre] = true;
 			}
 			cnt = 1;
 		}
 
-		if( i != sEnd.length() - 1 ){
+		if( i < len - 1 ){
 			if( cnt % k == 0 ){
-				sureBroken[cursor] = true;
+				broken[cursor] = true;
 			}
 		}
 		pre = cursor;
 	}
 
-	for( i=0; i<s.length(); i++ ) {
+	for( int i = 0; i < len; i++ ){
 		cursor = s[i];
-		if( sureNotBroken[cursor] == true ){
-			sureBroken[cursor] = false;
+		if( notBroken[cursor] == true ){
+			broken[cursor] = false;
 		}
 	}
 
-	for( i=0; i<s.length(); i++ ) {
+	for( int i = 0; i < len; i++ ){
 		cursor = s[i];
-		if( sureBroken[cursor] == true ){
-			if( printed.find(cursor) == printed.end() ){
+		if( broken[cursor] == true ){
+			if( printed[cursor] == false ){
 				cout<<cursor;
-				printed.insert(cursor);
+				printed[cursor] = true;
 			}
 		}
 	}
-	
-	cout<<"\n";
-
-	for( i=0; i<s.length(); ){
+	cout<<'\n';
+	for( int i = 0; i < len-1; ){
 		cursor = s[i];
 		cout<<cursor;
-		if( sureBroken[cursor] == true ){
-			i = i + k;
+		if( broken[cursor] == true ){
+			i += k;
 		}else{
-			i = i + 1;
+			i++;
 		}
 	}
-
+	cout<<'\n';
 	return 0;
 }
