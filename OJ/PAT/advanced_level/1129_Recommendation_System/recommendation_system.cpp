9f9219a36a0c3a258cda9079c462bd4e7864fba2
FrozenMap <ljjdbd123@hotmail.com>
2018-03-10 00:53:51 +0000

    update PAT_A_1129

--- OJ/PAT/advanced_level/1129_Recommendation_System/recommendation_system.cpp
+++ OJ/PAT/advanced_level/1129_Recommendation_System/recommendation_system.cpp
@@ -21,30 +21,36 @@ struct Item{
 };
 
 set<Item> s;
-// cntList[i] 表示id为i的商品，被用户点过的次数
-int cntList[MAXSIZE] = {0};
+// cnt_list[i] 表示id为i的商品，被用户点过的次数
+int cnt_list[MAXSIZE] = {0};
 
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int n, k, id;
-	cin>>n>>k;
-	for( int i = 0; i < n; ++i ){
+
+	int total, max_show, id;
+	cin>>total>>max_show;
+	for( int i = 0; i < total; ++i ){
 		cin>>id;
 		if( i != 0 ){
 			cout<<id<<":";
 			int j = 0;
-			for( auto it = s.begin(); it != s.end() && j < k; ++it, ++j ){
+			for( auto it = s.begin(); it != s.end() && j < max_show; ++it, ++j ){
 				cout<<' '<<it->id;
 			}
 			cout<<'\n';
 		}
-		auto it = s.find(Item( id, cntList[id] ) );		// 使用auto类型代替 set<Item>::iterator，更加简洁
+		// 更新 recommendation set
+		// 先erase，再insert
+		Item item_old(id, cnt_list[id]);
+		auto it = s.find(item_old);		// 使用auto类型代替 set<Item>::iterator，更加简洁
 		if( it != s.end() ){
 			s.erase(it);
 		}
-		cntList[id]++;
-		s.insert(Item( id, cntList[id] ) );
+		// 更新这个 id 的点击次数
+		cnt_list[id]++;
+		Item item_new(id, cnt_list[id]);
+		s.insert(item_new);
 	}
 	return 0;
 }
