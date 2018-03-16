85120752d923c7332810ed74df3ecfebf6154d35
FrozenMap <ljjdbd123@hotmail.com>
2018-03-16 01:45:52 +0000

    update_PAT_A_1106

--- OJ/PAT/advanced_level/1106_Lowest_Price_in_Supply_Chain/lowest_price_in_supply_chain.cpp
+++ OJ/PAT/advanced_level/1106_Lowest_Price_in_Supply_Chain/lowest_price_in_supply_chain.cpp
@@ -61,3 +61,81 @@ int main(){
 	cout<<setprecision(4)<<lowPrice<<' '<<minCnt<<'\n';
 	return 0;
 }
+
+/*
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#define MAXSIZE 100004
+// #define MAXSIZE 14
+using namespace std;
+
+struct Node {
+	int id, level;
+	vector<int> childList;
+	Node(){
+		level = 1;
+	}
+};
+
+int minLevel = MAXSIZE, cntMinLevel = 0;
+
+Node nodeList[MAXSIZE];
+
+// update level
+void levelTrav(){
+	queue<Node> q;
+	q.push(nodeList[0]);
+	while( q.size() ){
+		Node parent = q.front();
+		// vis(parent);
+		q.pop();
+		int level = parent.level;
+		if( parent.childList.size() == 0 ){
+			if( level < minLevel ){
+				minLevel = level;
+				cntMinLevel = 1;
+			}else if( level == minLevel ){
+				cntMinLevel++;
+			}
+		}else{
+			for( auto id : parent.childList ){
+				nodeList[id].level = level + 1;
+				q.push(nodeList[id]);
+			}
+		}
+
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int total;
+	double rawPrice, incRate;
+	cin>>total>>rawPrice>>incRate;
+
+	int k, id;
+	for( int i = 0; i < total; i++ ){
+		cin>>k;
+		nodeList[i].id = i;
+		for( int j = 0; j < k; j++ ){
+			cin>>id;
+			nodeList[i].childList.push_back(id);
+		}
+	}
+	levelTrav();
+
+	double minPrice = rawPrice * pow(1.0+incRate/100, minLevel-1);
+
+	cout<<fixed<<setprecision(4)<<minPrice;
+	cout<<' '<<cntMinLevel<<'\n';
+
+	return 0;
+}
+
+*/
