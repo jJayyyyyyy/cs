89def0517038bf8bd31cac65f5aef4c9abf8ab09
FrozenMap <ljjdbd123@hotmail.com>
2018-03-17 12:40:26 +0000

    update PAT_A_1121

--- OJ/PAT/advanced_level/1121_Damn_Single/damn_single.cpp
+++ OJ/PAT/advanced_level/1121_Damn_Single/damn_single.cpp
@@ -61,3 +61,83 @@ int main(){
 
 	return 0;
 }
+
+
+/*
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#define MAXSIZE 100004
+#define None -1
+using namespace std;
+
+struct Person{
+	int id, cpID;
+	bool atParty, cpAtParty;
+	Person(){
+		id = cpID = None;
+		atParty = cpAtParty = false;
+	}
+}
+
+Person personList[MAXSIZE];
+
+vector<int> coupleList(MAXSIZE, -1);
+vector<int> ans;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int cntCP, id1, id2;
+	cin>>cntCP;
+	for( int i = 0; i < cntCP; i++ ){
+		cin>>id1>>id2;
+		personList[id1].id = id1;
+		personList[id2].id = id2;
+
+		personList[id1].cpID = id2;
+		coupleList[id2].cpID = id1;
+	}
+
+	int guestCnt;
+	cin>>guestCnt;
+	for( int i = 0; i < guestCnt; i++ ){
+		int id, cpID;
+		cin>>id;
+
+		personList[id].atParty = true;
+		
+		cpID = personList[id].cpID;
+		if(cpID != None){
+			personList[cpID].cpAtParty = true;
+		}
+	}
+
+	for( int i = 0; i < MAXSIZE; i++ ){
+		if( guestList[i] == true ){
+			int id = i;
+			int cpid = coupleList[id];
+			if( cpid == -1 || guestList[cpid] == false ){
+				ans.push_back(id);
+			}
+		}
+	}
+
+	sort(ans.begin(), ans.end());
+	int len = ans.size();
+
+	cout<<len<<'\n';
+	if( len > 0 ){
+		cout<<setw(5)<<setfill('0')<<ans[0];
+		for( int i = 1; i < len; i++ ){
+			cout<<' '<<setw(5)<<setfill('0')<<ans[i];
+		}
+		cout<<'\n';
+	}
+
+	return 0;
+}
+*/
